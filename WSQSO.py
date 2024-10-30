878a944f8474cb19135262a5412bb95ac1f357c4
F4HTB <olivier@f4htb.fr>
2024-10-30 20:40:35 +0100

    modifications

--- WSQSO.py
+++ WSQSO.py
@@ -1,15 +1,99 @@
-import sys, random, re, configparser
+import sys, random, re, configparser, collections, time
 import numpy as np
+from datetime import datetime
 from PyQt6.QtWidgets import (
 	QApplication, QMainWindow, QWidget, QLabel, QLineEdit, QPushButton,
 	QTextEdit, QVBoxLayout, QHBoxLayout, QFormLayout, QMenu, QGridLayout,
-	QFrame, QCheckBox, QDialog, QDialogButtonBox, QRadioButton, QButtonGroup, QGroupBox, QMessageBox, QComboBox
+	QFrame, QCheckBox, QDialog, QDialogButtonBox, QRadioButton, QButtonGroup, QGroupBox, QMessageBox, QComboBox, 
+	QProgressBar
 )
-from PyQt6.QtGui import QAction, QActionGroup, QPainter, QColor, QPen, QImage
-from PyQt6.QtCore import Qt, QTimer
+from PyQt6.QtGui import QAction, QActionGroup, QPainter, QColor, QPen, QImage, QIcon
+from PyQt6.QtCore import Qt, QTimer, QThread, pyqtSignal
 from PyQt6.QtMultimedia import QAudioInput, QAudioFormat, QMediaDevices, QAudioSource
-import collections
 
+class TimerWorker(QThread):
+	# Signal to send updated time to the main thread
+	time_signal = pyqtSignal(int)
+
+	def run(self):
+		while True:
+			now = datetime.now()
+			ms_to_next_second = 1 - now.microsecond / 1_000_000.0
+			time.sleep(ms_to_next_second)
+			self.update_timer()
+
+	def update_timer(self):
+		now = datetime.now()
+		total_seconds = (now.minute % 2) * 60 + now.second
+		seconds_since_last_even_minute = total_seconds % 200
+		# Emit the signal with the updated timer value
+		self.time_signal.emit(seconds_since_last_even_minute)
+
+class FrequencyScaleWidget(QWidget):
+	def __init__(self, parent=None):
+		super().__init__(parent)
+		self.setMinimumWidth(50)  # Définir une largeur minimale pour l'échelle
+		self.shift_frequency = None  # Fréquence de décalage à afficher
+
+	def set_shift_frequency(self, frequency):
+		"""Met à jour la valeur de la shift frequency et force la mise à jour de l'affichage."""
+		min_freq = 1300
+		max_freq = 1700
+
+		# Assurez-vous que la fréquence est dans les limites de l'échelle
+		if frequency < min_freq:
+			frequency = min_freq
+		elif frequency > max_freq:
+			frequency = max_freq
+
+		self.shift_frequency = frequency
+		self.update()
+
+	def paintEvent(self, event):
+		painter = QPainter(self)
+		painter.setRenderHint(QPainter.RenderHint.Antialiasing)
+		painter.setPen(QPen(Qt.GlobalColor.black))  # Définir la couleur des traits en noir
+
+		# Taille du widget pour adapter la position des traits
+		widget_height = self.height()
+
+		# Limites des fréquences du spectre affiché (1300 à 1700 Hz)
+		min_freq = 1300
+		max_freq = 1700
+
+		# Pas de 50 Hz pour dessiner les traits
+		frequency_step = 50
+
+		# Dessiner les traits tous les 50 Hz
+		for frequency in range(min_freq, max_freq + 1, frequency_step):
+			# Calculer la position verticale correspondante sur le widget
+			y = widget_height - ((frequency - min_freq) / (max_freq - min_freq) * widget_height)
+
+			# Dessiner le trait (ligne)
+			painter.drawLine(0, int(y), 10, int(y))
+
+		# Dessiner les labels spécifiques pour 1400 Hz et 1600 Hz
+		for frequency in [1400, 1600]:
+			# Calculer la position verticale correspondante sur le widget
+			y = widget_height - ((frequency - min_freq) / (max_freq - min_freq) * widget_height)
+
+			# Dessiner le trait pour la fréquence spécifique
+			painter.drawLine(0, int(y), 10, int(y))
+
+			# Dessiner l'étiquette de la fréquence (label)
+			painter.drawText(15, int(y + 5), f"{int(frequency)} Hz")
+
+		# Dessiner le point rouge représentant la shift frequency, si elle est définie
+		if self.shift_frequency is not None:
+			# Calculer la position verticale du point rouge
+			y_shift = widget_height - ((self.shift_frequency - min_freq) / (max_freq - min_freq) * widget_height)
+
+			# Définir le pinceau pour dessiner en rouge
+			painter.setPen(QPen(Qt.GlobalColor.red))
+			painter.setBrush(Qt.GlobalColor.red)
+
+			# Dessiner un petit cercle rouge représentant la shift frequency
+			painter.drawEllipse(0, int(y_shift) - 5, 15, 4)  # Position et taille du cercle
 
 
 class WaterfallCanvas(QWidget):
@@ -20,6 +104,7 @@ class WaterfallCanvas(QWidget):
 		self.image = QImage(self.spectrogram_width, self.spectrogram_height, QImage.Format.Format_RGB32)
 		self.image.fill(Qt.GlobalColor.black)  # Remplir l'image avec du noir au démarrage
 		self.update()  # Forcer la mise à jour pour afficher le fond noir
+		self.should_draw_marker = False
 
 	def update_data(self, new_data):
 		# Convertir les nouvelles données FFT en intensités de couleur
@@ -40,10 +125,44 @@ class WaterfallCanvas(QWidget):
 			color = QColor(intensity, intensity, 255 - intensity)  # Couleur bleue dégradée
 			self.image.setPixel(self.spectrogram_width - 1, y, color.rgb())
 
+		if self.should_draw_marker:
+			# Définir le style du trait
+			pen = QPen(Qt.GlobalColor.white)
+			pen.setStyle(Qt.PenStyle.DotLine)
+			painter.setPen(pen)
+			
+			# Dessiner un trait en pointillé blanc sur toute la hauteur du canvas
+			painter.drawLine(self.spectrogram_width - 1, 0, self.spectrogram_width - 1, self.spectrogram_height)
+
+			# Ajouter l'heure et les minutes au marqueur
+			current_time = datetime.now().strftime("%Hh%M")
+			
+			# Sauvegarder la transformation actuelle pour la restaurer plus tard
+			painter.save()
+			
+			# Définir la transformation : translation et rotation
+			painter.translate(self.spectrogram_width - 10, self.spectrogram_height)
+			painter.rotate(-90)  # Faire pivoter de 90 degrés vers la gauche
+
+			# Dessiner le texte (à la verticale, aligné sur le bas)
+			painter.drawText(0, -5, current_time)  # Aligner le texte sur le bas
+
+			# Restaurer la transformation originale
+			painter.restore()
+
+			# Désactiver le marqueur après le dessin
+			self.should_draw_marker = False
+
+
 		# Terminer le dessin
 		painter.end()
 
 		# Mettre à jour l'affichage
+		self.update()
+
+	def draw_time_marker(self):
+		# Indiquer qu'il faut dessiner le trait marqueur
+		self.should_draw_marker = True
 		self.update()
 
 	def paintEvent(self, event):
@@ -192,7 +311,7 @@ class FrequencyShiftDialog(QDialog):
 		# Input field for fixed shift value
 		shift_value = parent.frequency_shift_value
 		self.frequency_shift_value_input = QLineEdit(str(shift_value))
-		self.frequency_shift_value_input.setPlaceholderText("Enter between 500-2500 Hz")
+		self.frequency_shift_value_input.setPlaceholderText("Enter between 1400-1600 Hz")
 		
 		shift_mode_group = QButtonGroup()
 		shift_mode_group.addButton(self.random_shift)
@@ -215,8 +334,8 @@ class FrequencyShiftDialog(QDialog):
 		if self.fixed_shift.isChecked():
 			try:
 				shift_value = int(self.frequency_shift_value_input.text().strip())
-				if shift_value < 500 or shift_value > 2500:
-					self.parent.show_error_message("Shift frequency must be between 500 and 2500 Hz.")
+				if shift_value < 1400 or shift_value > 1600:
+					self.parent.show_error_message("Shift frequency must be between 1400 and 1600 Hz.")
 					return
 			except ValueError:
 				self.parent.show_error_message("Shift frequency must be an integer.")
@@ -286,6 +405,7 @@ class WSPRQSOInterface(QMainWindow):
 		super().__init__()
 		self.setWindowTitle("WSPRQSO by F4HTB")
 		self.setGeometry(100, 100, 800, 600)
+		self.setWindowIcon(QIcon("icon.png"))
 		
 		# Initialize config parser
 		self.config = configparser.ConfigParser()
@@ -300,27 +420,27 @@ class WSPRQSOInterface(QMainWindow):
 		# Initialize default shift mode and value
 		self.shift_mode = self.config.get("Settings", "shift_mode", fallback="random")
 		if self.shift_mode == "random":
-			self.frequency_shift_value = random.randint(500, 2500)
+			self.frequency_shift_value = random.randint(1400, 1600)
 		else:
 			self.frequency_shift_value = int(self.config.get("Settings", "frequency_shift_value", fallback="1500"))
 	
 		# Band frequencies (MHz)
 		self.band_frequencies = {
-			"2200m": 0.1385,
-			"630m": 0.4752,
-			"160m": 1.8396,
-			"80m": 3.5696,
-			"60m": 5.2882,
-			"40m": 7.0411,
-			"30m": 10.1412,
-			"20m": 14.0981,
-			"17m": 18.1071,
-			"15m": 21.0971,
-			"12m": 24.9271,
-			"10m": 28.1271,
-			"6m": 50.2955,
-			"4m": 70.0920,
-			"2m": 144.4900
+			"2200m": 138500,
+			"630m": 475200,
+			"160m": 1839600,
+			"80m": 3569600,
+			"60m": 5288200,
+			"40m": 7041100,
+			"30m": 10141200,
+			"20m": 14098100,
+			"17m": 18107100,
+			"15m": 21097100,
+			"12m": 24927100,
+			"10m": 28127100,
+			"6m": 50295500,
+			"4m": 70092000,
+			"2m": 144490000
 		}
 
 		# Load selected band from config or set default
@@ -374,63 +494,139 @@ class WSPRQSOInterface(QMainWindow):
 		# Initialize UI components before setting band
 		main_layout = QGridLayout()
 		
-
+		#########
 		# Waterfall canvas pour afficher les spectres
 		self.canvas = WaterfallCanvas(self)
 		self.canvas.setMinimumSize(400, 273)  # Définir une taille minimale pour garantir la visibilité
-		main_layout.addWidget(self.canvas, 0, 0, 1, 2)
+		#########
+
+		#########
+		# Ajouter l'échelle au layout principal pour le canvas
+		self.scale_widget = FrequencyScaleWidget(self)
+		self.scale_widget.setMinimumWidth(70)  # Ajuster la largeur minimale de l'échelle
+		#########
 		
+		#########
 		# Frequency section with QGroupBox
-		freq_group = QGroupBox("Frequency")
-		freq_layout = QFormLayout()
-		
+		freq_group = QGroupBox("Frequency (Hz)")
+		freq_layout = QHBoxLayout()  # Utiliser un layout horizontal au lieu de QFormLayout
 		# Frequency Dial and TX with fixed graphical width corresponding to 10 characters
 		dial_label = QLabel("Dial:")
 		self.dial_input = QLineEdit()
 		self.dial_input.setMaxLength(10)
 		self.dial_input.setFixedWidth(100)  # Width fixed to fit 10 characters approximately
-		self.dial_input.textChanged.connect(self.update_tx_frequency)
-		
-		tx_label = QLabel("TX:")
+		self.dial_input.returnPressed.connect(self.update_tx_frequency)  # Connecter à une fonction pour gérer le changement de dial_input
+		# Input box pour le shift
+		shift_freq_label = QLabel(" + Δ :")
+		self.shift_freq_input = QLineEdit()
+		self.shift_freq_input.setMaxLength(10)
+		self.shift_freq_input.setFixedWidth(100)  # Taille adaptée
+		self.shift_freq_input.returnPressed.connect(self.update_tx_frequency)  # Connecter à une fonction pour gérer le changement de shift_freq_input
+		# TX Label and Input
+		tx_label = QLabel(" = TX:")
 		self.tx_input = QLineEdit()
 		self.tx_input.setMaxLength(10)
 		self.tx_input.setFixedWidth(100)  # Width fixed to fit 10 characters approximately
 		self.tx_input.setReadOnly(True)
+		# Ajouter les widgets au layout horizontal
+		freq_layout.addWidget(dial_label)
+		freq_layout.addWidget(self.dial_input)
+		freq_layout.addWidget(shift_freq_label)
+		freq_layout.addWidget(self.shift_freq_input)  # Ajouter la nouvelle input box de fréquence
+		freq_layout.addWidget(tx_label)
+		freq_layout.addWidget(self.tx_input)
+		# Ajouter un espace extensible à la fin pour pousser les widgets vers la gauche
+		freq_layout.addStretch()
+		# Appliquer le layout horizontal au QGroupBox
+		freq_group.setLayout(freq_layout)		
+		#########
 		
-		freq_layout.addRow(dial_label, self.dial_input)
-		freq_layout.addRow(tx_label, self.tx_input)
-		
-		freq_group.setLayout(freq_layout)
-		
+		#########
 		# Messages Display Area
 		self.message_display = QTextEdit()
 		self.message_display.setReadOnly(True)
+		#########
 		
+		#########
 		# Transmit Button
 		self.transmit_button = QPushButton("Transmit")
 		self.transmit_button.clicked.connect(self.transmit_message)
+		#########
 		
+		#########
+		# Barre de progression pour le temps
+		self.timer_progress = QProgressBar(self)
+		self.timer_progress.setRange(0, 200)  # Plage de 0 à 200 secondes
+		self.timer_progress.setValue(0)  # Valeur initiale
+		self.timer_progress.setFormat("%v s")  # Afficher les secondes (0 à 200) suivies de "s"
+
+		# Layout pour aligner la barre de progression en bas à droite
+		progress_layout = QHBoxLayout()
+		progress_layout.addStretch()  # Ajoute de l'espace flexible à gauche pour pousser la barre vers la droite
+		progress_layout.addWidget(self.timer_progress)
+		#########
+		
+		#########
+		# Créer un layout horizontal pour le canvas et l'échelle de fréquence
+		canvas_scale_layout = QHBoxLayout()
+		canvas_scale_layout.setSpacing(0)  # Enlever l'espace entre les widgets
+		canvas_scale_layout.addWidget(self.canvas, stretch=1)  # Le canvas occupe tout l'espace restant
+		canvas_scale_layout.addWidget(self.scale_widget)  # Ajouter l'échelle de fréquence juste à droite
+		# Définir une taille maximale pour scale_widget pour qu'il n'occupe pas plus de place que nécessaire
+		self.scale_widget.setMaximumWidth(100)  # Par exemple, une largeur de 100 pixels
+		#########
+
+		# Ajouter le layout horizontal au layout principal
+		
+		#########
 		# Organizing layouts in main layout
-		main_layout.addWidget(self.canvas, 0, 0, 1, 2)	  # Canvas in top left
-		main_layout.addWidget(freq_group, 1, 0)			 # Frequency controls in QGroupBox
-		main_layout.addWidget(self.transmit_button, 2, 0, 1, 2)  # Transmit button below inputs
-		main_layout.addWidget(QLabel("Message Log:"), 3, 0, 1, 2)
-		main_layout.addWidget(self.message_display, 4, 0, 1, 2)  # Message display area
-
+		# Ajouter les autres éléments de l'interface
+		main_layout.addLayout(canvas_scale_layout, 0, 0, 1, 3)  # Combiner les deux widgets sur toute la largeur (3 colonnes)
+		main_layout.addWidget(freq_group, 1, 0, 1, 3)  # Frequency controls in QGroupBox
+		main_layout.addWidget(self.transmit_button, 2, 0, 1, 3)  # Transmit button below inputs
+		main_layout.addWidget(QLabel("QSO Log:"), 3, 0, 1, 3)
+		main_layout.addWidget(self.message_display, 4, 0, 1, 3)  # Message display area
+		main_layout.addLayout(progress_layout, 5, 0, 1, 3)  # Ajouter à la fin du layout principal
 		main_widget.setLayout(main_layout)
-
-		# Set the selected band after initializing components
+		#########
+		
+		#########
+		# Update shift frequency pinter
+		self.scale_widget.set_shift_frequency(self.frequency_shift_value)
+		#########
+		
+		#########
+		# Set options after initializing components
 		for action in self.band_action_group.actions():
 			if action.text() == self.selected_band:
 				action.setChecked(True)
 				self.set_dial_frequency(self.band_frequencies[self.selected_band], self.selected_band)
-
-
+		self.shift_freq_input.setText(str(self.frequency_shift_value))
+		self.update_tx_frequency()
+		#########
+		
+		#########
+		# Create the TimerWorker and connect the signal
+		self.timer_worker = TimerWorker()
+		self.timer_worker.time_signal.connect(self.update_progress_bar)
+		self.timer_worker.start()
+		#########
+		
+		#########
 		# Initialiser un buffer circulaire pour les fréquences entre 1400 et 1600 Hz (par exemple)
 		#200 / (48000 / 32768) = 136.533 soit 137pix  * 60s * 2min = 16440
 		self.WSPRcircular_buffer = collections.deque(maxlen=(16440))  # Taille définie pour 2 minutes de données, ajustez selon vos besoins
+		#########
+		
+		#########
 		# Configuration pour l'audio
 		self.setup_audio()
+		#########
+	
+	def update_progress_bar(self, value):
+		self.timer_progress.setValue(value)
+		if value == 0:  # À chaque début de cycle de 200 secondes
+			self.canvas.draw_time_marker()
 
 	def setup_audio(self):
 		# Arrêter l'audio actuel si nécessaire
@@ -456,7 +652,7 @@ class WSPRQSOInterface(QMainWindow):
 				device = d
 				print("Selected device based on configuration:", d.description())
 				break
-
+		
 		# Si aucun périphérique correspondant n'est trouvé, utiliser le périphérique par défaut
 		if device is None:
 			device = QMediaDevices.defaultAudioInput()
@@ -499,13 +695,9 @@ class WSPRQSOInterface(QMainWindow):
 				# Mettre à jour le graphique avec les nouvelles données FFT
 				self.canvas.update_data(filtered_fft)
 				
-				print(f"Length of new_data: {len(filtered_fft)}") 
-				
 				# Filtrer les fréquences entre 1400 et 1600 Hz pour le buffer circulaire
 				specific_mask = (freqs >= 1400) & (freqs <= 1600)
 				specific_filtered_fft = np.abs(fft_result[specific_mask])
-
-				print(f"Length of new_data: {len(specific_filtered_fft)}")
 
 				# Ajouter les nouvelles données au buffer circulaire
 				self.WSPRcircular_buffer.extend(specific_filtered_fft)
@@ -527,7 +719,7 @@ class WSPRQSOInterface(QMainWindow):
 		line_edit.blockSignals(False)  # Réactiver les signaux
 	
 	def set_dial_frequency(self, frequency, band):
-		self.dial_input.setText(f"{frequency:.6f}")
+		self.dial_input.setText(f"{int(frequency)}")
 		self.selected_band = band
 		# Update the checkmark in the Band menu
 		for action in self.band_action_group.actions():
@@ -536,14 +728,11 @@ class WSPRQSOInterface(QMainWindow):
 	
 	def update_tx_frequency(self):
 		try:
-			dial_freq = float(self.dial_input.text())
-			# Use previously generated random shift value if random mode is active
-			shift_freq = self.frequency_shift_value
-			tx_freq = dial_freq + (shift_freq / 1e6)  # Convert shift from Hz to MHz
-			self.tx_input.setText(f"{tx_freq:.6f}")
+			tx_freq = int(self.dial_input.text()) + int(self.shift_freq_input.text())
+			self.tx_input.setText(f"{int(tx_freq)}")
 		except ValueError:
 			self.tx_input.setText("")
-	
+
 	def open_station_details(self):
 		dialog = StationDetailsDialog(self)
 		if dialog.exec() == QDialog.DialogCode.Accepted:
@@ -554,7 +743,7 @@ class WSPRQSOInterface(QMainWindow):
 		if dialog.exec() == QDialog.DialogCode.Accepted:
 			self.shift_mode = "random" if dialog.random_shift.isChecked() else "fixed"
 			self.frequency_shift_value = int(dialog.frequency_shift_value_input.text() or "1500")
-			self.update_tx_frequency()
+			self.shift_freq_input.setText(str(self.frequency_shift_value))
 
 	def open_audioconf_dialog(self):
 		dialog = AudioConfDialog(self)
@@ -564,7 +753,7 @@ class WSPRQSOInterface(QMainWindow):
 				# Utilisez le périphérique sélectionné, par exemple :
 				self.audio_device = selected_device
 				print(f"Selected audio device: {self.audio_device.description()}")
-				self.update_tx_frequency()
+
 	
 	def transmit_message(self):
 		self.message_display.append("Simulated message transmission.")
